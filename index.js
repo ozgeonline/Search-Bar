0bc16b03d37a0e616bf1aff8b13e99c3c04ffe41
ozgeonline <nisroc.414@hotmail.com>
2023-07-20 21:51:39 +0300

    Animated Search Bar
    with JS & CSS

---
+++ index.js
@@ -0,0 +1,6 @@
+const searchBarContainerIcon = document.querySelector(".search-bar-container");
+const magnifier = document.querySelector(".magnifier");
+
+searchBarContainerIcon.addEventListener("click" , ()=>{
+  searchBarContainerIcon.classList.toggle("active");
+})
